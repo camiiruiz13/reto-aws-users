6ea90558da1e0569db667dae337aeb462f80ff4d
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-07-21 14:49:43 -0500

    Proyecto srping boot para despliegue aws

---
+++ src/main/java/com/aws/ccamilo/com/app/retoawsusers/RetoAwsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ccamilo.com.app.retoawsusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAwsUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoAwsUsersApplication.class, args);
+    }
+
+}
